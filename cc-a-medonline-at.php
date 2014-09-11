3c89d711a099049639162b76d6e5aed30ac554f2
Frank Stuerzebecher <frank@hostz.at>
2014-09-11 13:03:22 +0200

    Cleanig up code.

--- cc-a-medonline-at.php
+++ cc-a-medonline-at.php
@@ -24,7 +24,7 @@ add_filter( 'pre_option_wpfb_css', 'amed_return_https' );
  * Make sure that page "/pneumo/" is delivered by https.
  */
 function amed_redirect_pneumo() {
-	if( $_SERVER['REQUEST_URI'] == '/pneumo/' && $_SERVER['SERVER_PORT'] == 80 ) {
+	if( strpos( $_SERVER['REQUEST_URI'], '/pneumo' ) !== false && $_SERVER['SERVER_PORT'] == 80 ) {
 		wp_redirect( 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] );
 		exit();
 	}
@@ -35,48 +35,56 @@ add_action( 'template_redirect', 'amed_redirect_pneumo');
 /**
  * Make the "Post Password Logout Button" appear in German.
  */
-function amed_make_postpass_button_german() {
+function amed_translate_postpass_button() {
 	// The plugin should be installed.
 	if( function_exists( 'pplb_logout_filter' ) ) {
 		function amed_adjust_postpass_logout_button( $content ) {
 			return str_replace( 'value="logout">', 'value="Abmelden"><br/><br/>', $content );
 		}
+
+		// Interferes with other password protected posts (Logoff not working).
+		if ( $_SERVER['REQUEST_URI'] != '/pneumo/' ) {
+			remove_filter( 'the_content', 'pplb_logout_filter', 9999, 1 );
+		}
+
 		// Because the plugin itself sets a filter priority of 9999 we have to overrule this.
 		add_filter( 'the_content', 'amed_adjust_postpass_logout_button', 10000 );
 	}
 }
-add_action( 'plugins_loaded', 'amed_make_postpass_button_german' );
+add_action( 'template_redirect', 'amed_translate_postpass_button' );
 
 
 /**
  * Avoid autocomplete in password fields of proteced posts.
  */
 function amed_secure_postpass_form ( $form ) {
-	return str_replace(
+
+	$needed_form = str_replace(
 		array(
 			'class="post-password-form"',
-			'type="password">'
+			'name="post_password"',
 		),
 		array(
 			'class="post-password-form" autocomplete="off"',
-			'type="password" autocomplete="off">'
+			'name="post_password" autocomplete="off"',
 		),
 		$form
 	);
+	return $needed_form;
 }
 add_filter( 'the_password_form', 'amed_secure_postpass_form');
 
 
 /**
- * Provide pluggable WP function to reset a post password cookie
+ * Provide pluggable WP function to renew the post password cookie
  * and add security attributes to it.
  */
 function wp_safe_redirect($location, $status = 302) {
 
-	// Added part: make the hardcoded WP cookie more secure (set in wp-login.php).
-	if( isset($GLOBALS['_GET']['action']) && $GLOBALS['_GET']['action'] == 'postpass') {
+	// Added part: make the hardcoded WP cookie "secure" and "httponly".
+	if( isset($_GET['action']) && $_GET['action'] == 'postpass' ) { // set in wp-login.php
 		global $hasher, $expire;
-		$ssl = is_ssl() ? true : false;
+		$ssl = ( strpos( $location, 'https://' ) === 0 ) ? true : false;
 		setcookie( 'wp-postpass_' . COOKIEHASH,
 			$hasher->HashPassword( wp_unslash( $_POST['post_password'] ) ),
 			$expire,
@@ -85,16 +93,15 @@ function wp_safe_redirect($location, $status = 302) {
 			$ssl, // secure
 			true  // httponly
 		);
-	} // End of edit
+		// Since we die here, the cookie has to be renewed already.
+		if( strlen($location) == 0 )
+			wp_die( 'Ihr Webbrowser scheint keinen Referer zu senden.<br/>Bitte verwenden Sie daher jetzt die Zur&uuml;ck-Schaltfl&auml;che Ihres Browsers und dr&uuml;cken Sie dann die Taste F5 auf Ihrer Tastatur ("Aktualisieren").' );
 
-
-// What follows is WP stuff.
+	} // End of added part - what follows is WP stuff.
 
 	// Need to look at the URL the way it will end up in wp_redirect()
 	$location = wp_sanitize_redirect($location);
-
 	$location = wp_validate_redirect($location, admin_url());
 	wp_redirect($location, $status);
 }
-
 
